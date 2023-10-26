f1229ee981d3f64eb8753ebf319a5cdbc8a7f094
matek10000 <matekofficial@interia.pl>
2023-10-26 18:53:25 +0200

    Wprowadzenie możliwości kodowania Szyfru Cezara

--- EncodeItFinal/CezarEncode.xaml.cs
+++ EncodeItFinal/CezarEncode.xaml.cs
@@ -23,5 +23,37 @@ namespace EncodeItFinal
         {
             InitializeComponent();
         }
+
+        private void cezar_but_Click(object sender, RoutedEventArgs e)
+        {
+            string input = cezar_text.Text.Replace(" ","");
+            if(int.TryParse(cezar_key.Text, out int input2))
+            {
+                string result = CezarEncoder(input, input2);
+                cezar_result.Content = result;
+            }
+            else
+            {
+                MessageBox.Show("Błędnie podany klucz!");
+                return;
+            }  
+        }
+
+        private string CezarEncoder(string input, int key) 
+        {
+            char[] inputArray = input.ToCharArray();
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                char c = inputArray[i];
+
+                if (char.IsLetter(c))
+                {
+                    char c2 = char.IsUpper(c) ? 'A' : 'a';
+                    inputArray[i] = (char)((c - c2 + key) % 26 + c2);
+                }
+            }
+            return new string(inputArray);
+        }
     }
 }
